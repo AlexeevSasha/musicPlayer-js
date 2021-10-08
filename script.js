259000dd3c3fde8221ee58a5e91469267539b645
AlexeevSasha <work.alexeevs@gmail.com>
2021-10-08 19:51:09 +0300

    add currentTimeEl and durationEl

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const songTitle = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -79,12 +83,38 @@ function nextSong() {
     playSong()
 }
 
-
-
-
 loadSong(songs[songIndex])
 
-//PrevBtn and nextBtn event listener
+function updateProgressBar(e) {
+   if(isPlaying) {
+       const { duration, currentTime} = e.srcElement;
+       //Update progress bar width
+       const progressPercent = (currentTime / duration) * 100;
+       progress.style.width = `${progressPercent}%`
+       //calculate display for duration
+       const durationMinute = Math.floor(duration / 60);
+       let durationSeconds =  Math.floor(duration % 60);
+       if(durationSeconds < 10) {
+        durationSeconds = `0${durationSeconds}`
+       }
+       if(durationSeconds) {
+        durationEl.textContent = `${durationMinute}:${durationSeconds}`
+       }
+       //calculate display for currentMinutes
+       const currentMinute = Math.floor(currentTime / 60);
+       let currentSeconds =  Math.floor(currentTime % 60);
+       if(currentSeconds < 10) {
+        currentSeconds = `0${currentSeconds}`
+       }
+       currentTimeEl.textContent = `${currentMinute}:${currentSeconds}`
+   }
+}
+
+//event listener
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+
+
+
 
